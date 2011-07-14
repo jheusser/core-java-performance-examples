c144fbfda3ae9e9f9b81381196bb0f3469cab90a
peter.lawrey <peter.lawrey@gmail.com>
2011-07-14 09:33:27 +0000

    Add support for ByteBufferText parser

--- src/main/java/com/google/code/java/core/parser/ByteBufferTextLongWriter.java
+++ src/main/java/com/google/code/java/core/parser/ByteBufferTextLongWriter.java
@@ -4,6 +4,7 @@ import java.nio.ByteBuffer;
 
 public class ByteBufferTextLongWriter implements LongWriter {
     private static final byte[] MIN_VALUE_TEXT = Long.toString(Long.MIN_VALUE).getBytes();
+    public static final char SEPARATOR = '\n';
     private final ByteBuffer buffer;
 
     public ByteBufferTextLongWriter(ByteBuffer buffer) {
@@ -22,15 +23,20 @@ public class ByteBufferTextLongWriter implements LongWriter {
         }
         if (num == 0) {
             writeByte('0');
-            writeByte('\n');
+            writeByte(SEPARATOR);
         } else {
+// find the number of digits
             int digits = ParserUtils.digits(num);
+// starting from the end, write each digit
             for (int i = digits - 1; i >= 0; i--) {
+                // write the lowest digit.
                 buffer.put(buffer.position() + i, (byte) (num % 10 + '0'));
+                // remove that digit.
                 num /= 10;
             }
+// move the position to after the digits.
             buffer.position(buffer.position() + digits);
-            writeByte('\n');
+            writeByte(SEPARATOR);
         }
     }
 
