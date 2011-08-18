943205ae99fa01438cbd54db79ab3bfd488ca56f
peter.lawrey <peter.lawrey@gmail.com>
2011-08-18 09:26:02 +0000

    Test to show thread safety.

---
+++ src/test/java/com/google/code/java/core/math/RoundingPerformanceMain.java
@@ -0,0 +1,62 @@
+package com.google.code.java.core.math;
+
+import java.math.BigDecimal;
+
+public class RoundingPerformanceMain {
+    public static void main(String... args) {
+        longCastPerf();
+        mathRoundPerf();
+        bidDecimalSetScalePerf();
+    }
+
+    private static double longCastPerf() {
+        double sum = 0; // to avoid micro-optimisation.
+        final int runs = 10 * 1000 * 1000;
+        long start = System.nanoTime();
+        for (int i = 0; i < runs; i++) {
+            double d = i * 0.01;
+            sum += roundToTwoPlacesCast(d);
+        }
+        long time = System.nanoTime() - start;
+        System.out.printf("Took an average of %,d ns for rounding using cast%n", time / runs);
+        return sum;
+    }
+
+    private static double mathRoundPerf() {
+        double sum = 0; // to avoid micro-optimisation.
+        final int runs = 10 * 1000 * 1000;
+        long start = System.nanoTime();
+        for (int i = 0; i < runs; i++) {
+            double d = i * 0.01;
+            sum += roundToTwoPlacesMath(d);
+        }
+        long time = System.nanoTime() - start;
+        System.out.printf("Took an average of %,d ns for rounding using Math.round%n", time / runs);
+        return sum;
+    }
+
+    private static double bidDecimalSetScalePerf() {
+        double sum = 0; // to avoid micro-optimisation.
+        final int runs = 1000 * 1000;
+        long start = System.nanoTime();
+        for (int i = 0; i < runs; i++) {
+            double d = i * 0.01;
+            sum += roundToTwoPlacesBigDecimal(d);
+        }
+        long time = System.nanoTime() - start;
+        System.out.printf("Took an average of %,d ns for rounding using BigDecimal.setScale%n", time / runs);
+        return sum;
+    }
+
+    public static double roundToTwoPlacesCast(double d) {
+        return ((long) (d < 0 ? d * 100 - 0.5 : d * 100 + 0.5)) / 100.0;
+    }
+
+    public static double roundToTwoPlacesMath(double d) {
+        return Math.round(d * 100) / 100.0;
+    }
+
+    public static double roundToTwoPlacesBigDecimal(double d) {
+        return new BigDecimal(d).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+}
